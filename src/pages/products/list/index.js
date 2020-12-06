cfb8b2cc92c3f93cd18b53a91e7f7f215acf58f5
Ilya Edelshtein <ilaltim@gmail.com>
2020-12-07 01:30:19 +0200

    products almost done need to fix problem with empty page

--- src/pages/products/list/index.js
+++ src/pages/products/list/index.js
@@ -3,6 +3,7 @@ import SortableTable from "../../../components/sortable-table";
 import headerpPoducts from "./products-list-header";
 import Router from "../../../router";
 import fetchJson from '../../../utils/fetch-json'
+import DoubleSlider from "../../../components/double-slider";
 export default class Page {
   element;
   subElements = {};
@@ -15,21 +16,120 @@ export default class Page {
 
     const input = this.element.querySelector('.form-label').nextElementSibling;
     input.addEventListener('input',this.onFilterInput);
+    document.addEventListener('pointerup', this.onSliderPointerUp);
+
+    const select = this.element.querySelector('.form-inline').lastElementChild.lastElementChild;
+    select.addEventListener('change', this.onStatusChange)
 
   }
+  onStatusChange = async (e) =>{
+    this.status = e.target.value;
+    await this.updateTableAfterFiltersChanges();
+  }
+  onSliderPointerUp = async (e) => {
+
+    if(!e.target.closest('.range-slider__inner')){
+      return;
+    }
+    const sliderElement = this.element.querySelector('.range-slider');
+    const leftSpanValue = sliderElement.firstElementChild.innerHTML;
+    this.priceStartFilter = this.remove$Sign(leftSpanValue);
+    const rightSpanValue = sliderElement.lastElementChild.innerHTML;
+    this.priceEndFilter = this.remove$Sign(rightSpanValue);
+    const data =  await this.updateTableAfterFiltersChanges();
+    this.components.sortableTable.updateForProductsPage(data);
+  }
+
   onFilterInput = async (e) =>{
     this.nameFilter = e.target.value
-    const url = new URL(`https://course-js.javascript.ru/api/rest/products`);
-    url.searchParams.set('_embed', 'subcategory.category');
-    url.searchParams.set('price_gte', this.priceStartFilter ? this.priceStartFilter : '0');
-    url.searchParams.set('price_lte', this.priceEndFilter ? this.priceEndFilter : '4000');
-    url.searchParams.set('title_like', `${this.nameFilter}`);
-    url.searchParams.set('_sort', 'title');
-    url.searchParams.set('_order', 'asc');
-    url.searchParams.set('_start', '0');
-    url.searchParams.set('_end', '30');
-    const data = await fetchJson(url);
-    this.components.sortableTable.updateForProductsPage(data);
+    const data =  await this.updateTableAfterFiltersChanges();
+    this.components.sortableTable.updateForProductsPage(data)
+  /*
+  _embed: subcategory.category
+price_gte: 85
+price_lte: 4000
+title_like: ч
+status: 1
+_sort: title
+_order: asc
+_start: 0
+_end: 30
+   */
+  }
+  remove$Sign($string){
+    const temp = $string.split('');
+    const result = [];
+    for(let i = 1 ; i< temp.length; i++){
+      result.push(temp[i]);
+    }
+    return result.join('');
+  }
+   updateTableAfterFiltersChanges = async () =>{
+     const url = new URL(`https://course-js.javascript.ru/api/rest/products`);
+     url.searchParams.set('_embed', 'subcategory.category');
+     url.searchParams.set('price_gte', this.priceStartFilter ? this.priceStartFilter : '0');
+     url.searchParams.set('price_lte', this.priceEndFilter ? this.priceEndFilter : '4000');
+     if(this.nameFilter){
+       url.searchParams.set('title_like', this.nameFilter);
+     }
+     url.searchParams.set('_sort', 'title');
+     if(this.status){
+       url.searchParams.set('status', this.status);
+     }
+     url.searchParams.set('_order', 'asc');
+     url.searchParams.set('_start', '0');
+     url.searchParams.set('_end', '30');
+     const data = await fetchJson(url);
+     if(!data.length){
+       const sortTable = this.element.querySelector('.sortable-table');
+       sortTable.className='sortable-table sortable-table_empty';
+
+
+       /*
+       <div>
+      <p>Не найдено товаров удовлетворяющих выбранному критерию</p>
+      <button type="button" class="button-primary-outline">Очистить фильтры</button>
+    </div>
+        */
+       const emptyPlaceHolder = this.element.querySelector('.sortable-table__empty-placeholder');
+       emptyPlaceHolder.innerHTML = `<div>
+      <p>Не найдено товаров удовлетворяющих выбранному критерию</p>
+      <button  type="button" class="button-primary-outline">Очистить фильтры</button>
+    </div>`
+
+       const btn = emptyPlaceHolder.lastElementChild.lastElementChild;
+       btn.addEventListener('click', async (e)=>{
+         this.status = null;
+         this.priceStartFilter = 0;
+         this.priceEndFilter = 4000;
+         this.nameFilter= null;
+         //Todo сбросить фильтер , спаны, и статус вылью
+         const input = this.element.querySelector('.form-label').nextElementSibling.value = ``;
+         const sliderElement = this.element.querySelector('.range-slider');
+         const leftSpanValue = sliderElement.firstElementChild.innerHTML=`$0`;
+         const rightSpanValue = sliderElement.lastElementChild.innerHTML=`$4000`;
+         this.components.doubleSlider.max = 4000;
+         this.components.doubleSlider.min = 0;
+         this.components.doubleSlider.update();
+        //TODO сделать новый запрос
+         const newurl = new URL(`https://course-js.javascript.ru/api/rest/products`);
+         url.searchParams.set('_embed', 'subcategory.category');
+         url.searchParams.set('price_gte',  '0');
+         url.searchParams.set('price_lte', '4000');
+         url.searchParams.set('_sort', 'title');
+         url.searchParams.set('_order', 'asc');
+         url.searchParams.set('_start', '0');
+         url.searchParams.set('_end', '30');
+         const data = await fetchJson(newurl);
+         emptyPlaceHolder.innerHTML = ``;
+         sortTable.className = 'sortable-table';
+         this.components.sortableTable.updateForProductsPage(data);
+
+       });
+
+     }else {
+       this.components.sortableTable.updateForProductsPage(data);
+     }
 
   }
 
@@ -51,19 +151,35 @@ export default class Page {
 
     element.innerHTML = this.getTemplate();
     this.element = element.firstElementChild;
+    //TODO создать дабл слайдер
+    const doubleSlider = new DoubleSlider({
+      min:0,
+      max:4000,
+      formatValue:value => '$' + value,
+      selected :{
+        from:0,
+        to:4000
+      }});
+    //TODO render слайдера происходит при вызове конструктора
+    //TODO добавить к элементу даблСлайдер.
+    const doubleSliderContainer = this.element.querySelector('.form-inline').firstElementChild.nextElementSibling
+    doubleSliderContainer.append(doubleSlider.element);
+    //TODO придумать обработчик событий на каждое взаимодейтсие со слайдером.
 
     //TODO создание sortableTable и первоначальный запрос
     const sortableTable = new SortableTable(headerpPoducts, {
       url: `api/rest/products?_embed=subcategory.category&_sort=title&_order=asc&_start=0&_end=30}`,
-      isSortLocally: false
+      isSortLocally: true
     });
 
-
+    //TODO добавить к элементу sortableTable
     this.element.append(sortableTable.element);
     this.components ={
-      sortableTable
+      sortableTable,
+      doubleSlider
     }
 
+    //TODO инициализировать листенеры
     this.initEvenListeners()
     return this.element;
   }
@@ -83,15 +199,8 @@ export default class Page {
           </div>
           <div class="form-group" data-elem="sliderContainer">
             <label class="form-label">Цена:</label>
-          <div class="range-slider">
-      <span data-elem="from">$0</span>
-      <div data-elem="inner" class="range-slider__inner">
-        <span data-elem="progress" class="range-slider__progress" style="left: 0%; right: 0%;"></span>
-        <span data-elem="thumbLeft" class="range-slider__thumb-left" style="left: 0%;"></span>
-        <span data-elem="thumbRight" class="range-slider__thumb-right" style="right: 0%;"></span>
-      </div>
-      <span data-elem="to">$4000</span>
-    </div></div>
+
+    </div>
           <div class="form-group">
             <label class="form-label">Статус:</label>
             <select class="form-control" data-elem="filterStatus">
@@ -110,6 +219,9 @@ export default class Page {
   }
 
   destroy() {
+    document.removeEventListener('pointerup', this.onSliderPointerUp);
+    document.removeEventListener('click',this.onItemClick);
+    document.removeEventListener('change',this.onStatusChange);
     for (const component of Object.values(this.components)) {
       component.destroy();
     }
